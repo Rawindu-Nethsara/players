7a8f9abc51edcbd54ebe9b68b0c2a31a8f56a78f
Rawindu-Nethsara <mooviedsited@gmail.com>
2025-09-15 02:39:31 -0700

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,6 @@
+const ads = [
+    "https://archive.org/details/aDs01.mp4",
+    "https://archive.org/details/aDs02.mp4",
+    "https://archive.org/details/aDs03.mp4"
+];
+const adTimes = [60, 2040, 3000]; // 1m, 34m, 50m in seconds
