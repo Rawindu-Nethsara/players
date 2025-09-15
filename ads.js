5bee8c676e69066bd0328a72d0151a33209fbf2b
Rawindu-Nethsara <mooviedsited@gmail.com>
2025-09-15 02:43:36 -0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,6 +1,6 @@
 const ads = [
-    "https://archive.org/details/aDs01.mp4",
-    "https://archive.org/details/aDs02.mp4",
-    "https://archive.org/details/aDs03.mp4"
+    "https://res.cloudinary.com/dyqw58yqs/video/upload/v1751187957/132140-752588954_small_o41b7g.mp4",
+    "https://res.cloudinary.com/dyqw58yqs/video/upload/v1751187957/132140-752588954_small_o41b7g.mp4",
+    "https://res.cloudinary.com/dyqw58yqs/video/upload/v1751187957/132140-752588954_small_o41b7g.mp4"
 ];
 const adTimes = [60, 2040, 3000]; // 1m, 34m, 50m in seconds
